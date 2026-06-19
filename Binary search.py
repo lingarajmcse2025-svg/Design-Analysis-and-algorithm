c76838a2202000344df1380ea24b3d4fe922ce76
lingarajmcse2025-svg <lingarajm.cse2025@citchennai.net>
2026-06-19 14:45:17 +0530

    Add files via upload

---
+++ Binary search.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target):
+    low=0
+    high=len(arr) - 1
+    while low <= high:
+        mid=(low + high)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1  
+sorted_numbers=[1, 2, 3, 4, 7, 9, 12, 15]
+target_value=int(input("Enter the number :"))
+result = binary_search(sorted_numbers,target_value)
+if result != -1:
+    print(f"Element found at index: {result}")
+else:
+    print("Element not found in the list.")
+    
